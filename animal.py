49ffcff5f4e59461953047070acde1be48351ca3
yasithkeshan <yasithkeshan4444@gmail.com>
2024-10-18 15:32:15 +0530

    Added a cat

--- animal.py
+++ animal.py
@@ -1,10 +1,17 @@
 import sys
 
+def cat():
+    print("Meow")
+
+
 def default():
     print("Hello")
 
 def main():
-    default()
+    if sys.argv[1] == 'cat':
+        cat()
+    else:    
+        default()
 
 if __name__ == '__main__':
     main()
